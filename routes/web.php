23ef81c4da541f3fa7e8365cedde33f18b20d3d5
Md. Arif Hassan <aarifhsn@gmail.com>
2025-04-09 23:35:05 +0600

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,7 @@
 use App\Http\Controllers\AIController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/ai', [AIController::class, 'index'])->name('ai.index');
+Route::get('/', [AIController::class, 'index'])->name('ai.index');
 Route::middleware(['throttle:5,1'])
     ->post('/ai/generate', [AIController::class, 'generateText'])
     ->name('ai.generate');
